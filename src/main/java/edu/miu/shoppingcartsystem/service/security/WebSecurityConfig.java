28c112a338eb6a62dec7be2048a2bbfee3b5a54f
nahid <nahid.imtiaz19@gmail.com>
2023-04-16 16:21:12 -0500

    open only product get api

--- src/main/java/edu/miu/shoppingcartsystem/service/security/WebSecurityConfig.java
+++ src/main/java/edu/miu/shoppingcartsystem/service/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import edu.miu.shoppingcartsystem.service.security.services.UserDetailsServiceIm
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -83,7 +84,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-        .antMatchers("/api/test/**").permitAll().antMatchers("/products/**").permitAll()
+        .antMatchers("/api/test/**").permitAll().antMatchers(HttpMethod.GET, "/products/**").permitAll()
         .anyRequest().authenticated();
     
     http.authenticationProvider(authenticationProvider());
