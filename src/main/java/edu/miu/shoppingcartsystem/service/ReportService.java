4cb6581d47838a4f5426b27d8300f0c3e5eb82e3
sushanta-ach <sushantaacharjeebd@gmail.com>
2023-04-19 15:53:47 -0500

    Update ReportService.java
    report service

--- src/main/java/edu/miu/shoppingcartsystem/service/ReportService.java
+++ src/main/java/edu/miu/shoppingcartsystem/service/ReportService.java
@@ -1,4 +1,32 @@
 package edu.miu.shoppingcartsystem.service;
 
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ReportService {
+    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+        String path = "C:\\Users\\FLORA PC\\Documents\\report";
+        List<Product> employees = repository.findAll();
+        //load file and compile it
+        File file = ResourceUtils.getFile("classpath:report.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Sushanta Acharjee");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\report.html");
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\report.pdf");
+        }
+
+        return "report generated in path : " + path;
+    }
 }
