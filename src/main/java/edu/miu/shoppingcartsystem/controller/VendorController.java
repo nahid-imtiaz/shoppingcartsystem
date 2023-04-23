aba3e288aae634fc580adf7e1bb4fddd6b3582d3
sushanta-ach <sushantaacharjeebd@gmail.com>
2023-04-23 15:10:39 -0500

    Update vendor Controller

--- src/main/java/edu/miu/shoppingcartsystem/controller/VendorController.java
+++ src/main/java/edu/miu/shoppingcartsystem/controller/VendorController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,27 +18,27 @@ public class VendorController {
     @Autowired
     private VendorService vendorService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Vendor>> getAllVendors() {
         List<Vendor> vendors = vendorService.getAllVendors();
         return ResponseEntity.ok(vendors);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(path="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Vendor> getVendorById(@PathVariable Long id) {
         Optional<Vendor> vendor = vendorService.getVendorById(id);
         return vendor.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasRole('VENDOR') or hasRole('ADMIN')")
     public ResponseEntity<Vendor> saveVendor(@RequestBody Vendor vendor) {
         Vendor savedVendor = vendorService.saveVendor(vendor);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedVendor);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping(path="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    // @PreAuthorize("hasRole('VENDOR') or hasRole('ADMIN')")
     public ResponseEntity<Vendor> updateVendor(@PathVariable Long id, @RequestBody Vendor vendor) {
         Optional<Vendor> existingVendor = vendorService.getVendorById(id);
         if (existingVendor.isPresent()) {
@@ -49,8 +50,8 @@ public class VendorController {
         }
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
-    //@PreAuthorize("hasRole('VENDOR') or hasRole('ADMIN')")
     public ResponseEntity<Void> deleteVendorById(@PathVariable Long id) {
         Optional<Vendor> vendor1 = vendorService.getVendorById(id);
         if (vendor1.isPresent()) {
