15b947e1adc0bcd565906150f778961bebae9225
Abedur Rahman <abedur@gmail.com>
2023-04-23 10:45:14 -0500

    Cart Item JUnit test
    JUnit test

--- src/test/java/edu/miu/shoppingcartsystem/ShoppingcartsystemApplicationTests.java
+++ src/test/java/edu/miu/shoppingcartsystem/ShoppingcartsystemApplicationTests.java
@@ -1,13 +1,33 @@
 package edu.miu.shoppingcartsystem;
 
+import edu.miu.shoppingcartsystem.model.CartItem;
+import edu.miu.shoppingcartsystem.repository.CartItemRepository;
+import edu.miu.shoppingcartsystem.service.CartItemService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ShoppingcartsystemApplicationTests {
+	@Autowired
+	private CartItemService cartItemService;
+
+	@MockBean
+	private CartItemRepository cartItemRepository;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void getCartItemById(){
+		int id = 1;
+		//when(cartItemRepository.findById(id)).thenReturn(Stream.of(new CartItem()))
 	}
 
 }
