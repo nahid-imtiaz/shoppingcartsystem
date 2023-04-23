3ad97873757f1245778500a35a2b51a7be5cda19
sushanta-ach <sushantaacharjeebd@gmail.com>
2023-04-23 16:33:00 -0500

    ok

--- src/main/java/edu/miu/shoppingcartsystem/repository/VendorRepository.java
+++ src/main/java/edu/miu/shoppingcartsystem/repository/VendorRepository.java
@@ -2,6 +2,7 @@ package edu.miu.shoppingcartsystem.repository;
 
 import edu.miu.shoppingcartsystem.model.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
